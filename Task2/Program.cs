72a922f23608a937cdb14302fb31c3502e5919f9
Aleksey Myakinin <amykis@mail.ru>
2024-01-11 11:51:55 +0300

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача №1: Создать массив и заполнить его целыми числами от 1 до N
+// (N - произвольное натуральное число). Вывести элементы массива на экран.
+
+int n = 10;
+int[] arr = new int[n];
+int i = 0;
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(arr[' ']);
+    Console.Write($"{arr[i]} ");
+    i += 1;
+}
