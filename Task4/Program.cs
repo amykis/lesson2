36e4b474af54f4519edaec01906c748eb2b67c87
Aleksey Myakinin <amykis@mail.ru>
2024-01-11 18:08:18 +0300

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача №3: Дано N гирь с разным весом в случайном порядке.
+// Определить вес самой тяжелой гири.
+
+int n = 5;
+int[] array = { 2, 4, 7, 3, 9 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i += 1;
+}
+Console.WriteLine(max);
